436c8768ee58dde58b344ecc403c1ef3f3097fb2
Marcel Šulek <marcel@sulek.eu>
2013-09-25 16:20:27 +0200

    updatovan topojson generujici prikaz

--- data/topojson.cmd
+++ data/topojson.cmd
@@ -1,1 +1,2 @@
 topojson -o okresy.json okresy_wgs84.shp -q 1e4 -s 1.5e-8 -p nuts=KODNUTS
+topojson -o obce.topojson mesta=./mc/mestskeCasti_wgs84_UTM33.shp obce=./obce/obce.shp -q 1e4 -s 1e-9 -p id=ICZUJ -p name=NAZOB -p namemc=NAZMC --shapefile-encoding=cp1250
