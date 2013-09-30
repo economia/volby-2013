f4f2f03a9f9776963dc1660f53a03bda7eefec1c
Marcel Šulek <marcel@sulek.eu>
2013-09-30 15:54:15 +0200

    pripsan prikaz pro vygenerovani topojsonu pro rok 98 a 96

--- data/topojson.cmd
+++ data/topojson.cmd
@@ -1,2 +1,3 @@
 topojson -o okresy.json okresy_wgs84.shp -q 1e4 -s 1.5e-8 -p nuts=KODNUTS
 topojson -o obce.topojson mesta=./mc/mestskeCasti_wgs84_UTM33.shp obce=./obce/obce.shp -q 1e4 -s 1e-9 -p id=ICZUJ -p name=NAZOB -p namemc=NAZMC --shapefile-encoding=cp1250
+topojson -o obce_98.topojson mesta=./mc98/mestskeCasti_wgs84_UTM33.shp obce=./obce98/obce.shp -q 1e4 -s 1e-9 -p id=ICZUJ -p name=NAZOB -p namemc=NAZMC --shapefile-encoding=cp1250
